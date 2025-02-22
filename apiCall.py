bcd11e6ecf894e44105e4293d24629bf4ccb21b0
Daniel Han <dhan17@syr.edu>
2025-02-22 14:47:01 -0500

    Basic data functions on dataset

--- apiCall.py
+++ apiCall.py
@@ -4,4 +4,23 @@ import numpy as np
 
 df = pd.read_csv("SYRCityline_Requests_(2021-Present)(1).csv")
 
-print(df.head())
+
+# Checking if dataset is valid
+def checkDataset(): 
+    print(df.head())
+    print(df.tail())
+
+
+def dfInfo(): 
+    # Descriptive stats
+    print(df.describe())
+    # Column names, associated data types
+    print(df.info())
+    # Check for any null values     
+    print(df.isnull().sum())
+
+
+checkDataset()
+dfInfo()
+
+
