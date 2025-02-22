266ce22dbe69f0287c5621907fa169881413bf66
Sahaj <ssoni02@syr.edu>
2025-02-22 18:34:59 -0500

    long and lat functions

--- zoning.py
+++ zoning.py
@@ -1,13 +1,31 @@
 import pandas as pd
 import numpy as np
 
+df = pd.read_csv("SYRCityline_Requests_(2021-Present)(1).csv")
+df2 = pd.read_csv("Parking_Violations_-_2023_-_Present(1).csv")
+
 # Determine the overlap in the range of Longitude values between the Cityline dataset
 def detLongBounds() -> list:
-    print("")
+    minLongCity = df['Lng'].min()
+    minLongPV = df2['LONG'].min()
+    maxLongCity = df['Lng'].max()
+    maxLongPV = df2['LONG'].max()
+    
+    long_list = [max(minLongCity, minLongPV), min(maxLongCity, maxLongPV)]
+
+    return long_list
 
 # Determine the overlap in the range of Latitude values between the Cityline dataset
 def detLatBounds() -> list:
-    print("")
+    minLatCity = df['Lat'].min()
+    minLatPV = df2['LAT'].min()
+    maxLatCity = df['Lat'].max()
+    maxLatPV = df2['LAT'].max()
+
+    lat_list = [max(minLatCity, minLatPV), min(maxLatCity, maxLatPV)]
+
+    return lat_list
+
 
 
 # x axis is the number of cityline complaints, y axis is the number of parking violations 
@@ -41,5 +59,7 @@ def zones(minLong, maxLong, minLad, maxLad,numOfHorizontalDivs: int, numOfVertic
 
 
 
+print(detLatBounds())
+print(detLongBounds())
 
 zones(10, 100, 10, 100, 7, 7)
