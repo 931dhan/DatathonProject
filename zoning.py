a7ffbcc02a33536ccbdcb2e5ad5160b4de22c7c0
Daniel Han <dhan17@syr.edu>
2025-02-22 16:27:05 -0500

    Zoning function outline

---
+++ zoning.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Determine the overlap in the range of Longitude values between the Cityline dataset
+def detLongBounds() -> list:
+    print("")
+
+# Determine the overlap in the range of Latitude values between the Cityline dataset
+def detLatBounds() -> list:
+    print("")
+
+
+# x axis is the number of cityline complaints, y axis is the number of parking violations 
+# Given a num of horizontal and vertical divisions, determine the cubic meters of each area by performing conversioin of lat and long to meters. 
+# Returns list of objects, each object includes lat, long ranges.
+def zones(numOfHorizontalDivs, numOfVerticalDivs) -> list[object]:
+    print("")
