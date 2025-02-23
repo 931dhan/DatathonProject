610093e2a1baff631dd0c72ed18ce9a5643674ca
Daniel Han <dhan17@syr.edu>
2025-02-22 22:17:19 -0500

    updating our dataset

--- buildDataset.py
+++ buildDataset.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
 import zoning
+import os
 
 
 df = pd.read_csv("Parking_Violations_-_2023_-_Present(1).csv")
@@ -11,17 +12,59 @@ def numberOfParkingViolations(minLat, maxLat, minLong, maxLong):
                  & (df['LONG'] >= minLong) & (df['LONG'] <= maxLong)]
     return len(filterd)
 
-print(numberOfParkingViolations(df["LAT"].min(), df["LAT"].max(), df["LONG"].min(), df["LONG"].max()))
-print(len(df))
-
-
 def numberOfCityComps(minLat, maxLat, minLong, maxLong) -> int: 
     filterd = df2[(df2['Lat'] >= minLat) & (df2['Lat'] <= maxLat)
                 & (df2['Lng'] >= minLong) & (df2['Lng'] <= maxLong)]   
     return len(filterd)
 
-print(numberOfCityComps(df2["Lat"].min(), df2["Lat"].max(), df2["Lng"].min(), df2["Lng"].max()))
-print(len(df2))
+
+def StreetsSidewalksRequests(minLat, maxLat, minLong, maxLong):
+    filterd = df2[(df2['Lat'] >= minLat) & (df2['Lat'] <= maxLat)
+                & (df2['Lng'] >= minLong) & (df2['Lng'] <= maxLong)
+                & (df2['Agency_Name'] == "Streets, Sidewalks & Transportation")]   
+    return len(filterd)
+
+def GarbRecyGraffRequests(minLat, maxLat, minLong, maxLong):
+    filterd = df2[(df2['Lat'] >= minLat) & (df2['Lat'] <= maxLat)
+                & (df2['Lng'] >= minLong) & (df2['Lng'] <= maxLong)
+                & (df2['Agency_Name'] == "Garbage, Recycling & Graffiti")]   
+    return len(filterd)
+
+def HousingPropertyRequests(minLat, maxLat, minLong, maxLong):
+    filterd = df2[(df2['Lat'] >= minLat) & (df2['Lat'] <= maxLat)
+                & (df2['Lng'] >= minLong) & (df2['Lng'] <= maxLong)
+                & (df2['Agency_Name'] == "Housing & Property Maintenance")]   
+    return len(filterd)
+
+def FeedbackToCityRequests(minLat, maxLat, minLong, maxLong):
+    filterd = df2[(df2['Lat'] >= minLat) & (df2['Lat'] <= maxLat)
+                & (df2['Lng'] >= minLong) & (df2['Lng'] <= maxLong)
+                & (df2['Agency_Name'] == "Feedback to the City")]   
+    return len(filterd)
+
+def ParkingVehiclesRequests(minLat, maxLat, minLong, maxLong):
+    filterd = df2[(df2['Lat'] >= minLat) & (df2['Lat'] <= maxLat)
+                & (df2['Lng'] >= minLong) & (df2['Lng'] <= maxLong)
+                & (df2['Agency_Name'] == "Parking & Vehicles")]   
+    return len(filterd)
+
+def GreenSpacesRequests(minLat, maxLat, minLong, maxLong):
+    filterd = df2[(df2['Lat'] >= minLat) & (df2['Lat'] <= maxLat)
+                & (df2['Lng'] >= minLong) & (df2['Lng'] <= maxLong)
+                & (df2['Agency_Name'] == "Green Spaces, Trees & Public Utilities")]   
+    return len(filterd)
+
+def WaterSewageRequests(minLat, maxLat, minLong, maxLong):
+    filterd = df2[(df2['Lat'] >= minLat) & (df2['Lat'] <= maxLat)
+                & (df2['Lng'] >= minLong) & (df2['Lng'] <= maxLong)
+                & (df2['Agency_Name'] == "Water & Sewage")]   
+    return len(filterd)
+
+def AnimalsRequests(minLat, maxLat, minLong, maxLong):
+    filterd = df2[(df2['Lat'] >= minLat) & (df2['Lat'] <= maxLat)
+                & (df2['Lng'] >= minLong) & (df2['Lng'] <= maxLong)
+                & (df2['Agency_Name'] == "Animals")]   
+    return len(filterd)
 
 
 # Column 1, 2 should be long range, lat range from arrZones object respectively
@@ -41,7 +84,16 @@ def buildDataSet(arrZones):
             "latRange" : zone["latRange"], 
             "longRange" : zone["longRange"], 
             "numOfCityComp" : numberOfCityComps(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1]), 
-            "numOfParkingViol" : numberOfParkingViolations(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1])
+            "numOfParkingViol" : numberOfParkingViolations(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1]),
+            "numOfTransportReq" : StreetsSidewalksRequests(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1]),
+            "numOfGarbageRecycleGraffitiReq" : GarbRecyGraffRequests(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1]),
+            "numOfHousingPropertyReq" : HousingPropertyRequests(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1]),
+            "numOfCityFeedbackReq" : FeedbackToCityRequests(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1]),
+            "numOfParkingVehiclesReq" : ParkingVehiclesRequests(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1]),
+            "numOfGreenSpaces" : GreenSpacesRequests(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1]),
+            "numOfWaterSewageReq" : WaterSewageRequests(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1]),
+            "numOfAnimalsReq" : AnimalsRequests(zone["latRange"][0], zone["latRange"][1], zone["longRange"][0], zone["longRange"][1]),
+
         }
         arrOfRows.append(row)
 
@@ -49,4 +101,13 @@ def buildDataSet(arrZones):
     return pd.DataFrame(arrOfRows)
 
 
-print(buildDataSet(createArrZones(10, 10)))
+
+# ourDf = buildDataSet(createArrZones(50, 50))
+# ourDf2 = ourDf[ourDf["numOfParkingViol"] > 1000]
+
+
+cwd = os.getcwd()
+print(cwd)
+# path = cwd + "/new"
+# ourDf2.to_csv(path)
+
