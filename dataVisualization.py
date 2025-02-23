59e5bf92193d132f438b293edabeffa7042c8122
Daniel Han <dhan17@syr.edu>
2025-02-23 12:17:11 -0500

    update visualization

--- dataVisualization.py
+++ dataVisualization.py
@@ -6,13 +6,17 @@ import plotly
 import plotly.graph_objects as go
 
 df = pd.read_csv("ourDataset.csv")
+df2 = pd.read_csv("ourDatasetWithCategories.csv")
 
 agencyArr = (list(df))
 
-fig = go.Figure(data=[go.Pie(labels=agencyArr[5:], 
-                             values=df[agencyArr[5:]].sum()
-                             )
-                             ])
+def requestsByAgency():
+    fig = go.Figure(data=[go.Pie(labels=agencyArr[5:], 
+                                values=df[agencyArr[5:]].sum()
+                                )
+                                ])
+    fig.show()
 
-fig.show()
 
+# def requestsByAgencyInZone(latRange, longRange):
+#     fig = go.Figure(data = )
